f141d3b5205cab90a202ef49a05acfeb1070dc4a
Benjamin Boucher-Charest <47901054+BenjaminB-Charest@users.noreply.github.com>
2024-11-03 16:55:21 -0500

    Adding snetiment analysis to deployment

--- frontend/src/components/Sentiments.jsx
+++ frontend/src/components/Sentiments.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import DataTable from "./Datatable";
 import { Text } from "@chakra-ui/react";
-import { useDataContext } from "../contexts/DataContext";
-
+import SentimentContainer from "./SentimentContainer";
+const mock_response = {
+  body: "{\"message\": \"[{\\\"ticker\\\": \\\"AAPL\\\", \\\"hedgeFundTrendValue\\\": -386045483.0, \\\"smartScore\\\": 7, \\\"bloggerSectorAvg\\\": 0.6901, \\\"bloggerBullishSentiment\\\": 0.74, \\\"insidersLast3MonthsSum\\\": -22602362.0, \\\"newsSentimentsBearishPercent\\\": 0.1026, \\\"newsSentimentsBullishPercent\\\": 0.8974, \\\"investorHoldingChangeLast7Days\\\": -0.00108054984247974, \\\"investorHoldingChangeLast30Days\\\": 0.0095256980618674, \\\"priceTarget\\\": 245.84, \\\"convertedPriceTarget\\\": 245.84, \\\"convertedPriceTargetCurrencyId\\\": 1, \\\"fundamentalsReturnOnEquity\\\": 1.574125, \\\"fundamentalsAssetGrowth\\\": 0.03516, \\\"technicalsTwelveMonthsMomentum\\\": 0.2878, \\\"sma\\\": \\\"Positive\\\", \\\"analystConsensus\\\": \\\"ModerateBuy\\\", \\\"hedgeFundTrend\\\": \\\"Decreased\\\", \\\"insiderTrend\\\": \\\"SoldShares\\\", \\\"investorSentiment\\\": \\\"Neutral\\\", \\\"newsSentiment\\\": \\\"VeryBullish\\\", \\\"bloggerConsensus\\\": \\\"Bullish\\\", \\\"marketCountryId\\\": 1, \\\"isomic\\\": \\\"XNAS\\\"}]\"}",
+  statusCode: 200
+};
 const Sentiments = (props) => {
-  const { state } = useDataContext();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   const ticker = props.symbol;
 
   useEffect(() => {
@@ -20,28 +21,34 @@ const Sentiments = (props) => {
         })
         .then((data) => {
           console.log(data);
-          setData(data);
+          parsedResponse = JSON.parse(mock_response.body);
+          const finalData = JSON.parse(parsedResponse.message);
+          setData(finalData);
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
         });
+        /*const parsedResponse = JSON.parse(mock_response.body);
+
+        // Step 2: Parse the inner JSON in `message`
+        const finalData = JSON.parse(parsedResponse.message);
+        console.log(finalData[0]);
+        setData(finalData[0]);*/
+            
     }
   }, [ticker]);
 
   return (
     <div style={{ overflowX: "hidden", overflowY: "scroll", height: "600px" }}>
-      <div
-        style={{
-          width: "50%",
-          height: "100%",
-          backgroundColor: "rgba(255,255,255,0.5)",
-          borderRadius: "10px",
-        }}
-      >
         <Text style={{ marginLeft: "10px" }} fontSize="2xl" mb={4}>
-          Investor Sentiment
+          Sentiments
         </Text>
-      </div>
+        <div style={{display:"flex", flexDirection:"row", width:"100%"}}>
+                <SentimentContainer name="Retail"   consensus={ data?.bloggerConsensus }/>
+                <SentimentContainer name="Investors"  consensus={ data?.investorSentiment }/>
+                <SentimentContainer name="News media" consensus={ data?.newsSentiment } />          
+        </div>
+                 
     </div>
   );
 };
