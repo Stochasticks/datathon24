9106ef33128d0eac2831fcaa83c762df28166b21
Benjamin Boucher-Charest <47901054+BenjaminB-Charest@users.noreply.github.com>
2024-11-03 23:19:51 -0500

    Completed first part of sentiment analysis

--- frontend/src/components/SentimentContainer.jsx
+++ frontend/src/components/SentimentContainer.jsx
@@ -12,11 +12,24 @@ const SentimentContainer = (props) => {
                 borderRadius: "10px",
                 margin: "0 4px"}}
         >
-                  <Text fontSize="1.5xl" mb={4} mt={4} style={{ width: "100%", textAlign: "center", fontWeight: "500" }} >{props.name}</Text>   
-                  <Text style={{ width: "100%", textAlign: "center" }}><span style={{fontWeight:"bold"}}>Consensus :</span> <span style={{fontWeight:"500",color:props.consensus.color}}>{props.consensus.label}</span></Text>
-                <div>
-                        
-                </div>            
+                  <Text fontSize="2xl" mb={4} mt={4} style={{ width: "100%", textAlign: "center", fontWeight: "bold" }} >{props.name}</Text>  
+                  <div style={{display:'flex',flexDirection:'column', width:"100%", justifyContent:'center',alignItems:"center", alignContent:"center"}}>
+                    <Text style={{textAlign: "center", fontWeight:"bold"}}>Consensus</Text>
+                    <div style={{display: "flex", width: "100px",height: "100px",backgroundColor: props.consensus.color, borderRadius: "50%", justifyContent:'center', alignItems:'center'}}>
+                      <Text color={'white'} style={{fontWeight:"bold", fontSize:"14px"}}>{props.consensus.label}</Text>
+                    </div>
+                  </div>
+                <div style={{height:'8px'}}/>
+                {
+                  props.bullishScore > 0 ? (
+                    <div>
+                      <Text style={{ textAlign: "center", fontWeight: "bold" }}>Percentage bullish</Text>
+                      <Text style={{ fontSize: "30px", fontWeight: "bold", textAlign: "center" }}>
+                        {props.bullishScore}
+                      </Text>
+                    </div>
+                  ) : null
+                }
       </div>
   );
 };
